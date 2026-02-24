33cae5ce0d857381dd040bcf336a6af1ac233842
Mahsa_Rz <mahsaawb.r@gmail.com>
2026-02-23 19:56:41 -0500

    articles added

---
+++ frontend/src/data/articles.js
@@ -0,0 +1,19 @@
+// src/data/articles.js
+export const articles = [
+    { 
+      id: "anchoring-bias", 
+      title: "The Anchoring Effect", 
+      excerpt: "Why the first number you see controls your decision.", 
+      readTime: "5 min", 
+      tags: ["Neuroscience", "Biases"],
+      content: "The anchoring effect is a cognitive bias where an individual depends too heavily on an initial piece of information offered (considered to be the 'anchor') to make subsequent judgments during decision making. Once the value of this anchor is set, all future negotiations, arguments, estimates, etc. are discussed in relation to the anchor. Information that aligns with the anchor tends to be assimilated toward it, while information that is more dissonant or less related tends to be displaced."
+    },
+    { 
+      id: "framing-bias", 
+      title: "The Framing Effect", 
+      excerpt: "How presentation changes our tolerance for risk.", 
+      readTime: "10 min", 
+      tags: ["Biases", "HCI"],
+      content: "The framing effect creates a cognitive bias where people decide on options based on whether the options are presented with positive or negative connotations; e.g. as a loss or as a gain. People tend to avoid risk when a positive frame is presented but seek risks when a negative frame is presented."
+    }
+  ];
